9c13459b4cf5a3c56ac83641db60e0571cbaeb88
VanessaMaia26 <vanessamaia2607@gmail.com>
2024-03-15 15:10:24 -0300

    settings

---
+++ src/test/java/br/com/vanessamaia/primeiroprojetospringboot/PrimeiroProjetoSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vanessamaia.primeiroprojetospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroProjetoSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
